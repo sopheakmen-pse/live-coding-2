208f237a42911fd31185b3dc5901d193967702ab
MochNoch <sreymoch.lorn@institute.pse.ngo>
2024-03-20 16:51:59 +0700

    live coding

---
+++ lorn-sreymoch/index.js
@@ -0,0 +1,63 @@
+// variables & Data Types
+
+let name = "Vannda";
+
+let age  = 24;
+
+const isStudent = Yes;
+
+let grades = [1,2,3];
+
+let username = "vannda"
+
+let score = 99.9;
+
+const isLoggedln = Yes;
+
+let emails = [sopheak.men@pse.ngo, siemhong.em@pse.ngo];
+
+let userlnfo = {id:(1), username:("vannda"), isAcivated:("ture")};
+
+let productName = "IPhone X";
+
+let price = 15;
+
+const isActive = Yes;
+
+let companyName = "sala";
+
+const hasSubscription = No;
+
+let fruitsList = [banana , apple , pineapple , mango];
+
+// If eles
+
+if student = "pt" , 90 = "Excellent"{
+    else 70 = "Goode job"
+}
+
+// Function
+
+function calculateRectangleArea = ("length" , "whidth") {
+    let = ("length" * "whidth" )
+    length = 10 ;
+    whidth = 20 ;
+    return length * whidth 
+};
+
+function fruitsList = ("b","m")
+if fruits
+
+// Object
+
+const student1 ={fullname:"Kosal Vong", age: 18, gender: "male", province: Takeo};
+const student2 ={fullname:"Savanna Tep", age: 14, gender: "female", province: Battambong};
+
+// Array
+
+const students = [
+    {"Kosal Vong",18,"male",Takeo}
+    {"Savanna Tep", 14,"female",Battambong};
+];
+
+
