aa5f2b3e5de2d9890cbb0abe0d999e055f509136
ThornThavann <thavann.thorn@institute.pse.ngo>
2024-03-20 17:10:32 +0700

    live cording

---
+++ index.js
@@ -0,0 +1,111 @@
+// variable & Date Type
+let name = "Vannda";
+let age = 24;
+let isStudent = yes;
+let grades =[1, 2, 3];
+
+let username = "Vannda";
+let number = 99.9;
+let isLoggedIn = yes;
+let emails =["sopheakmen@pse.ngo", "siemhong.em@pse.ngo"];
+
+const userInfo(1) ={
+let username = vannda", 
+let isActivated = true,
+}
+
+let productName = "IPhone X";
+let price = 15;
+let isActive = Yes;
+let companyName = "Sala";
+let hasSubscription = No;
+let fruitsList =["banana","apple", "pineapple", "mango"];
+
+// If Else
+let studentScore = 70;
+If (studentScore <90){
+   getting ="Excellent!";
+
+   console.log ("Excellent!");
+} else if (studentScore <70){
+    greeting = "Goode job!";
+
+    console.log ("Goode job");
+  }else  (studentScore >70){
+    greeting = "Goode job!";
+
+    console.log ("Need improvement!");
+  }
+
+
+
+
+// Functions
+
+
+function calculateRectangleArea(a, b) {
+  return a * b;
+}
+
+let pricedate ()= {
+    {banana = $1},
+    {mango = $2},
+
+};
+
+function price() {
+    let pricedate= "Volvo";
+    return banana ;
+  }
+
+
+
+function getTotalPrice(a, b) {
+  return a * b;
+}
+
+function add() {
+    const x = 2,2;
+    const y = 1,3;
+    return x ;
+  }
+
+//   Objects
+
+const Student1={
+  name: "My name is Kosal Vong",
+  age: "I am 18 years old",
+  gender: "I am MathMLElement",
+  Born: "I was born in Takeo province",
+};
+
+const Student2={
+    name: "My name is Savanna Tep",
+    age: "I am 14 years old",
+    gender: "I am female",
+    Born: "I was born in Battambong province",
+  };
+
+
+
+
+
+//   Array
+const Students=[
+    { name: "My name is Kosal Vong", age: "I am 18 years old", gender: "I am MathMLElement", Born: "I was born in Takeo province"},  
+    { name: "My name is Savanna Tep", age: "I am 14 years old", gender: "I am female", Born: "I was born in Battambong province"},
+]
+
+    // loop
+
+document.getElementById("text").innerHTML=
+
+let student=[name,],
+
+for (let i = 0; i < 3; i++) {
+  if (students[i].students === "text") {
+    text.push(students[i]);
+  }
+}
+
+console.log(text);
