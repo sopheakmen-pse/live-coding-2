014929d1acdfe8d51985e67e2705bae17ad2be79
sreypheaphorn <sreyphea.phorn@institute.pse.ngo>
2024-05-06 11:37:19 +0700

    Test skill 2.1

--- start/Phorn Sreyphea/index.js
+++ start/Phorn Sreyphea/index.js
@@ -1,35 +1,34 @@
-// let studentName = "sreyphea";
-// let age = 17;
-// let isStudent = true;
-// let isteacher = false;
-// let subjects = ["English", "Enviroment", "life skill"];
-// let schoolName = "SOB";
-// let classroomNumber = 13;
-// let isMorningClass = true;
-// let isOtherwise = false;
-// let friends = ["marika", "sreymey", "sopheak"];
-// let teacher = { id: "0001", fullname: "Em Siemhong", age: 34, Experience: 10 };
-// let favoriteSubject = "English";
+let studentName = "sreyphea";
+let age = 17;
+let isStudent = true;
+let isteacher = false;
+let subjects = ["English", "Enviroment", "life skill"];
+let schoolName = "SOB";
+let classroomNumber = 13;
+let isMorningClass = true;
+let isOtherwise = false;
+let friends = ["marika", "sreymey", "sopheak"];
+let teacher = { id: "0001", fullname: "Em Siemhong", age: 34, Experience: 10 };
+let favoriteSubject = "English";
 
-// function getTotalPrice(nbOfBanana, nbOfOrange) {
-//     let pricebanana = 0.5;
-//     let priceorage = 2;
-//     let totalbanana = nbOfBananaanana * Pricebanana;
-//     const totalorange = nbOfOrange * Priceorange;
-//     const totalPrice = totalbanana + totalorange;
+function getTotalPrice(nbOfBanana, nbOfOrange) {
+    let pricebanana = 0.5;
+    let priceorage = 2;
+    let totalbanana = nbOfBananaanana * Pricebanana;
+    const totalorange = nbOfOrange * Priceorange;
+    const totalPrice = totalbanana + totalorange;
+    return totalPrice;
+}
+function gettotalprice() {
+    let getTotalPrice1=(2, 2);
+    let getTotalPrice2 =(1, 3);
+}
+console.log(getTotalPrice)
 
-//     return totalPrice;
-// }
-// function gettotalprice() {
-//     let getTotalPrice1=(2, 2);
-//     let getTotalPrice2 =(1, 3);
-// }
-// console.log(getTotalPrice)
 
-
-let weatherData = document.getElementById(location);
-
-{data.location.country}
+let weatherData = {
+ index.inerHTML()
+}
 console.log(weatherData)
 
 
